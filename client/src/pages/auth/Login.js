ed19609636c140df87246a007b48fc136edea415
BKsodhi <sodhibhavnoor29@gmail.com>
2026-05-21 18:14:29 +0530

    Fix hardcoded localhost in Login.js

--- client/src/pages/auth/Login.js
+++ client/src/pages/auth/Login.js
@@ -364,7 +364,8 @@ const Login = () => {
 
     try {
       const res = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        // "http://localhost:5000/api/auth/login",
+        "/api/auth/login",
         { email, password }
       );
 
